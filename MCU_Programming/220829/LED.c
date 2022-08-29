f6622a725e00111e0f63002c9e92294a343a921f
kmkim036 <kmkim036@gmail.com>
2022-08-29 13:15:13 +0900

    rename

--- MCU_Programming/220829/LED.c
+++ MCU_Programming/220829/LED.c
@@ -24,7 +24,7 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
  * IN THE SOFTWARE.
  *********************************************************************************************************************/
-#include "LEDdefine.h"
+#include "user_define.h"
 
 #define LED
 

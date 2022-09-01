3457cf434c957327fd7b2d792ee2161f05a089c6
kmkim036 <kmkim036@gmail.com>
2022-09-01 12:53:42 +0900

    Add: pseudo code

---
+++ Embedded_based_Software_Project/pseudo_code.c
@@ -0,0 +1,73 @@
+// game 1
+while (1) {
+    LED random on;
+    timer start;
+    if (timer > 1.5s || uncorrect switch) {
+        //
+    } else {
+        win_cnt++
+    }
+    switch (win_cnt) {
+    case 0:
+        no led;
+        break;
+    case 1:
+        red rgb led;
+        break;
+    case 2:
+        green rgb led;
+        break;
+    case 3:
+        blue rgb led;
+        break;
+    case 4:
+        white rgb led;
+        break;
+    }
+    while (1)
+        ;
+}
+
+// game 2
+mode = 1 or 0;
+
+while (1) {
+    while (1) {
+        if (mode == 1) {
+            RGB GREEN;
+        if (reached?) {
+            game win;
+            LED BLUE;
+            break;
+        }
+        } else {
+            save distance;
+            RGB RED;
+            if (distance changed?) {
+                game lose;
+                LED RED;
+                break;
+            }
+        }
+    }
+}
+
+timer interrupt
+{
+    mode toggle;
+    3 seconds;
+}
+
+// game 3
+while (1) {
+    random target potentiometer setting;
+    while (1) {
+        if (potentiometer > target value) {
+            RGB BLUE;
+        } else {
+            RGB RED;
+            break;
+        }
+    }
+    //
+}

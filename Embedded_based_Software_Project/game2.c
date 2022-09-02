0e84d96ab05fe2f1a2b8441766da65580460384b
kmkim036 <kmkim036@gmail.com>
2022-09-02 21:33:58 +0900

    add: buzzer

--- Embedded_based_Software_Project/game2.c
+++ Embedded_based_Software_Project/game2.c
@@ -1,9 +1,7 @@
-// ********************************************************** //
-// need to add buzzer code
-
 #include "user_define.h"
 
 #define TAG_THREHSHOLD 5
+#define PWM_FREQ 6250000
 
 IfxCpu_syncEvent g_cpuSyncEvent = 0;
 
@@ -149,6 +147,72 @@ void init_CCU60(void)
     /* CCU60 T12 Start */
     CCU60_TCTR4 = (1 << T12RS); // T12 starts counting
 }
+void init_Buzzer(void)
+{
+    PORT02_IOCR0 &= ~((0x1F) << PC3); // PORT02.3 : Alternate output function 1 (push-pull)
+    PORT02_IOCR0 |= ((0x11) << PC3); // PORT02.3 : GTM_TOUT3
+}
+
+void init_GTM_TOM0_PWM(void)
+{
+    /* GTM Enable */
+    // Password Access to unlock WDTCPU0CON0
+    SCU_WDT_CPU0CON0 = ((SCU_WDT_CPU0CON0 ^ 0xFC) & ~(1 << LCK)) | (1 << ENDINIT);
+    while ((SCU_WDT_CPU0CON0 & (1 << LCK)) != 0)
+        ;
+
+    // Modify Access to clear ENDINIT bit
+    SCU_WDT_CPU0CON0 = ((SCU_WDT_CPU0CON0 ^ 0xFC) | (1 << LCK)) & ~(1 << ENDINIT);
+    while ((SCU_WDT_CPU0CON0 & (1 << LCK)) == 0)
+        ;
+
+    GTM_CLC &= ~(1 << DISR); // Enable GTM Module
+
+    // Password Access to unlock WDTCPU0CON0
+    SCU_WDT_CPU0CON0 = ((SCU_WDT_CPU0CON0 ^ 0xFC) & ~(1 << LCK)) | (1 << ENDINIT);
+    while ((SCU_WDT_CPU0CON0 & (1 << LCK)) != 0)
+        ;
+
+    // Modify Access to set ENDINIT bit
+    SCU_WDT_CPU0CON0 = ((SCU_WDT_CPU0CON0 ^ 0xFC) | (1 << LCK)) | (1 << ENDINIT);
+    while ((SCU_WDT_CPU0CON0 & (1 << LCK)) == 0)
+        ;
+
+    while ((GTM_CLC & (1 << DISS)) != 0)
+        ; // Wait until module is enabled
+
+    /* GTM Clock Setting */
+    GTM_CMU_FXCLK_CTRL &= ~((0xF) << FXCLK_SEL); // Input clock of CMU_FXCLK : CMU_GCLK_EN
+
+    GTM_CMU_CLK_EN &= ~((0x3) << EN_FXCLK); // Enable all CMU_FXCLK
+    GTM_CMU_CLK_EN |= ((0x2) << EN_FXCLK);
+
+    /* GTM TOM0 PWM Setting */
+    GTM_TOM0_TGC1_GLB_CTRL &= ~((0x3) << UPEN_CTRL3); // TOM0 channel 11 enable update of
+    GTM_TOM0_TGC1_GLB_CTRL |= ((0x2) << UPEN_CTRL3); // register CM0, CM1, CLK_SRC
+
+    GTM_TOM0_TGC1_FUPD_CTRL &= ~((0x3) << FUPD_CTRL3); // Enable force update of TOM0 channel 11
+    GTM_TOM0_TGC1_FUPD_CTRL |= ((0x2) << FUPD_CTRL3);
+    GTM_TOM0_TGC1_FUPD_CTRL &= ~((0x3) << RSTCN0_CH3); // Reset CN0 of TOM0 channel 11 on force update
+    GTM_TOM0_TGC1_FUPD_CTRL |= ((0x2) << RSTCN0_CH3);
+
+    GTM_TOM0_TGC1_ENDIS_CTRL &= ~((0x3) << ENDIS_CTRL3); // Enable channel 11 on an update trigger
+    GTM_TOM0_TGC1_ENDIS_CTRL |= ((0x2) << ENDIS_CTRL3);
+
+    GTM_TOM0_TGC1_OUTEN_CTRL &= ~((0x3) << OUTEN_CTRL3); // Enable channel 11 output on an update trigger
+    GTM_TOM0_TGC1_OUTEN_CTRL |= ((0x2) << OUTEN_CTRL3);
+
+    GTM_TOM0_CH11_CTRL |= (1 << SL); // High signal level for duty cycle
+    GTM_TOM0_CH11_CTRL &= ~((0x7) << CLK_SRC_SR); // Clock source : CMU_FXCLK(1) = 6250 kHz
+    GTM_TOM0_CH11_CTRL |= (1 << CLK_SRC_SR);
+
+    GTM_TOM0_CH11_SR0 = 12500 - 1; // PWM freq. = 6250 kHz / 12500 = 500 Hz
+    GTM_TOM0_CH11_SR1 = 0; // Duty cycle = 0
+
+    GTM_TOUTSEL0 &= ~((0x3) << SEL3); // TOUT3 : TOM0 channel 11
+
+    GTM_TOM0_TGC1_GLB_CTRL |= (1 << HOST_TRIG); // Trigger request signal to update
+}
 
 int core0_main(void)
 {
@@ -170,6 +234,8 @@ int core0_main(void)
     init_CCU60();
     init_LED();
     init_RGBLED();
+    init_Buzzer();
+    init_GTM_TOM0_PWM();
 
     irq_ultra_sensor = 0;
 
@@ -184,6 +250,15 @@ int core0_main(void)
             if (distance < TAG_THREHSHOLD && distance != 0) {
                 PORT10_OMR = (1 << PS2); // LED BLUE ON
                 PORT10_OMR = (1 << PCL1); // LED RED OFF
+
+                int pwm_cnt = PWM_FREQ / 261;
+                GTM_TOM0_CH11_SR0 = pwm_cnt;
+                GTM_TOM0_CH11_SR1 = pwm_cnt / 2;
+                GTM_CMU_CLK_EN |= ((0x2) << EN_FXCLK); // enable
+                for (int j = 0; j < 50000000; j++)
+                    ;
+                GTM_CMU_CLK_EN &= ~((0x2) << EN_FXCLK); // disable
+
                 break;
             }
         } else {
@@ -195,6 +270,15 @@ int core0_main(void)
                 if (distance_now - distance_cmp > 10 || distance_cmp - distance_now > 10) {
                     PORT10_OMR = (1 << PCL2); // LED BLUE OFF
                     PORT10_OMR = (1 << PS1); // LED RED ON
+
+                    int pwm_cnt = PWM_FREQ / 392;
+                    GTM_TOM0_CH11_SR0 = pwm_cnt;
+                    GTM_TOM0_CH11_SR1 = pwm_cnt / 2;
+                    GTM_CMU_CLK_EN |= ((0x2) << EN_FXCLK); // enable
+                    for (int j = 0; j < 50000000; j++)
+                        ;
+                    GTM_CMU_CLK_EN &= ~((0x2) << EN_FXCLK); // disable
+
                     break;
                 }
             }
